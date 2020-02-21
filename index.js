80f9a91a7ae4f484e64c64ba1957ae06af317581
Chad.Snyder <Mapletonkid@gmail.com>
2020-02-21 10:54:48 -0700

    spaced server console return

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./server');
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, ()=>{
-  console.log(`Running and listening on port: ${PORT}`);
+  console.log(`\n\n ***Running and listening on port: ${PORT}*** \n\n`);
 })
